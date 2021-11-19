3ba9c9797f8f2d40a76a6ec34ef36bf2ad57f480
LucasLassiter <53961896+LucasLassiter@users.noreply.github.com>
2021-11-19 14:43:25 -0800

    Delete src directory

--- src/animals/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Animal, AnimalPicture, Tag
-
-# Register your models here.
-admin.site.register(Animal)
-admin.site.register(AnimalPicture)
-admin.site.register(Tag)
